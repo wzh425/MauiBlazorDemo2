e47b008cc79f3461177a2e6921f2eef1c225f566
wzh425 <wuzihao425@outlook.com>
2022-09-28 19:17:32 +0800

    添加项目文件。

---
+++ MauiBlazorDemo/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiBlazorDemo;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ MauiBlazorDemo/Pages/Index.razor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#if IOS
+using IOS.WeChat;
+using UIKit;
+#endif
+
+namespace MauiBlazorDemo.Pages
+{
+    public partial class Index
+    {
+        private void Test()
+        {
+#if IOS
+            WXApi.RegisterApp("wx3f94d21d67c4ae76");
+            SendMessageToWXReq req = new SendMessageToWXReq();
+            req.Text = "213";
+            req.BText = true;
+            req.Scene = 1;
+            WXApi.SendReq(req);
+#endif
+        }
+    }
+}
